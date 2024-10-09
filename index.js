4683e2e44309cbf8e3c0a59397b07e8536ce27ea
cchuuu <chuyuanz@nycstudents.net>
2024-10-09 12:26:36 -0400

    a

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ function formSubmittion() {
 formSubmittion();
 
 const DOMSelectors = {
-  button: document.querySelector("#button1"),
+  button: document.getElementById("button1"),
+  container: document.getElementById("container"),
+  inputs: document.getElementById("#input"),
 };
 
-console.log(DOMSelectors.button);
+function addingText() {
+  DOMSelectors.button.addEventListener("click", function () {
+    DOMSelectors.container.insertAdjacentText = DOMSelectors.texts;
+  });
+}
+addingText();
